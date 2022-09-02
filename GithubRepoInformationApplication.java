4801a11e159a088b0c8e7e4c768757a46f29fee8
vishnu1494 <vishnubabu14@gmail.com>
2022-09-02 13:36:06 +0200

    Uploading Code Base

---
+++ GithubRepoInformationApplication.java
@@ -0,0 +1,17 @@
+package com.githubrepo.githubrepoinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Git Repo Information", version = "2.0", description = "Git repo Information"))
+public class GithubRepoInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepoInformationApplication.class, args);
+	}
+
+}
